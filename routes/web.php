5a4d415f2ec025e0ae41b391bc046b036926e14f
Charan Kolaparthi <saicharan4466@gmail.com~>
2022-03-30 21:49:01 +0530

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\HomeController;
+
+use App\Http\Controllers\AdminController;
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+route::get('/redirect',[HomeController::class,'redirect']);
+
+
+route::get('/',[HomeController::class,'index']);
+
+route::get('/product',[AdminController::class,'product']);
+
+route::post('/uploadproduct',[AdminController::class,'uploadproduct']);
+
+route::get('/showproduct',[AdminController::class,'showproduct']);
+
+route::get('/deleteproduct/{id}',[AdminController::class,'deleteproduct']);
+
+route::get('/search',[HomeController::class,'search']);
+
+route::post('/addcart/{id}',[HomeController::class,'addcart']);
+
+route::get('/showcart',[HomeController::class,'showcart']);
+
+route::get('/delete/{id}',[HomeController::class,'deletecart']);
+
+route::post('/order',[HomeController::class,'confirmorder']);
